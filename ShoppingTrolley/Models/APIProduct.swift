d27c045a6feb4173c94e3a1ad88a6ea5aac3bf04
Grace Couch <gracetcouch@gmail.com>
2025-07-01 17:36:54 +0100

    api string and product model

---
+++ ShoppingTrolley/Models/APIProduct.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  ShoppingTrolley
+//
+//  Created by Grace couch on 01/07/2025.
+//
+import Foundation
+
+struct APIProduct {
+    let id: Int
+    let title: String
+    let price: Float
+    let description: String
+    let category: String
+    let image: String
+}
---
+++ ShoppingTrolley/Services/FakeStoreAPIService.swift
@@ -0,0 +1,12 @@
+//
+//  FakeStoreAPIService.swift
+//  ShoppingTrolley
+//
+//  Created by Grace couch on 01/07/2025.
+//
+
+import Foundation
+
+class FakeStoreAPIService {
+    let apiURLString = "https://fakestoreapi.com/products"
+}
