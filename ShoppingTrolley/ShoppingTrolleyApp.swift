82732c8203828c3a9a308d612f178c1fcd5c6f3a
Grace Couch <gracetcouch@gmail.com>
2025-07-01 13:17:41 +0100

    initial commit

---
+++ ShoppingTrolley/ShoppingTrolleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingTrolleyApp.swift
+//  ShoppingTrolley
+//
+//  Created by Grace couch on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingTrolleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShoppingTrolleyTests/ShoppingTrolleyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingTrolleyTests.swift
+//  ShoppingTrolleyTests
+//
+//  Created by Grace couch on 01/07/2025.
+//
+
+import Testing
+@testable import ShoppingTrolley
+
+struct ShoppingTrolleyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
