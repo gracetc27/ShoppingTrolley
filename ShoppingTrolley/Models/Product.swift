bb496d6d8ae66cfa74acf79d9dfee9d547f2b3d7
Grace Couch <gracetcouch@gmail.com>
2025-07-03 17:05:24 +0100

    adds view interacting product

---
+++ ShoppingTrolley/Models/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  ShoppingTrolley
+//
+//  Created by Grace couch on 03/07/2025.
+//
+
+struct Product: Identifiable {
+    let id: Int
+    let title: String
+    let price: Float
+    let description: String
+    let category: String
+    let image: String
+}
