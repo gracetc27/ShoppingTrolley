91ae3d56f8b1a724e11a3533f4cbfee91c42c2c3
Grace Couch <gracetcouch@gmail.com>
2025-07-03 17:06:02 +0100

    testing the network request

---
+++ ShoppingTrolley/ViewModels/ProductViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  ProductViewModel.swift
+//  ShoppingTrolley
+//
+//  Created by Grace couch on 03/07/2025.
+//
+
+import Foundation
+
+@Observable
+class ProductViewModel {
+    var service = FakeStoreAPIService()
+    var products: [Product] = []
+
+    func getProducts() async {
+        do throws(APIError) {
+            try await self.products = service.getProducts().map { apiProduct in
+                Product(
+                    id: apiProduct.id,
+                    title: apiProduct.title,
+                    price: apiProduct.price,
+                    description: apiProduct.description,
+                    category: apiProduct.category,
+                    image: apiProduct.image)
+            }
+        } catch {
+            print(error)
+        }
+
+    }
+}
---
+++ ShoppingTrolley/Views/ProductsListView.swift
@@ -0,0 +1,24 @@
+//
+//  ProductsListView.swift
+//  ShoppingTrolley
+//
+//  Created by Grace couch on 03/07/2025.
+//
+
+import SwiftUI
+
+struct ProductsListView: View {
+    @State private var vm = ProductViewModel()
+    var body: some View {
+        List(vm.products) { product in
+            Text(product.title)
+        }
+        .task {
+           await vm.getProducts()
+        }
+    }
+}
+
+#Preview {
+    ProductsListView()
+}
